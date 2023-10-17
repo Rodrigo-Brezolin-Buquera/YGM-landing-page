9517a9775fbabef5bf7fbaa76d7687d267789d6b
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2023-10-17 16:53:37 -0300

    ajuste profile imag

--- src/sections/teachers/TeacherCard.js
+++ src/sections/teachers/TeacherCard.js
@@ -14,7 +14,8 @@ export const TeacherCard = ({ image, description, name }) => {
             w={"100%"}
             align="center"
             backgroundColor={"brand.100"}
-        >
+        >      
+       
                 <Image
                     src={image}
                     alt={`${name} - Foto perfil`} 
@@ -22,14 +23,11 @@ export const TeacherCard = ({ image, description, name }) => {
                     height={150}         
                     style={{
                         objectFit: 'cover',
-                        width: "150px",
-                        height: "150px",
-                        objectPosition: '50% 50%',
-                        borderRadius:'50%'
+                        borderRadius:'50%'                   
                     }}
                 />
          
-            <Stack>
+            <Stack width={["100%", "80%"]} >
                 <CardBody>
                     <Heading color={"brand.400"} size='lg' textAlign={["center", "initial"]}>{name}</Heading>
                     <Text py='2' color={"brand.400"} textAlign={["center", "initial"]}>
